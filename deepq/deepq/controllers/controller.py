f6d8295c3ef5c5d3c7e93a97d245b2df241bab98
erik <erik.schultheis@stud.uni-goettingen.de>
2017-04-04 17:49:15 +0200

    simple episode tracking

--- deepq/deepq/controllers/controller.py
+++ deepq/deepq/controllers/controller.py
@@ -2,6 +2,27 @@ import tensorflow as tf
 from ..action_space import ActionSpace
 from .memory import History
 
+class EpisodeTracker(object):
+    def __init__(self):
+        self._return = 0
+        self._length = 0
+
+    def __call__(self, reward):
+        self._return += reward
+        self._length += 1
+
+    @property
+    def duration(self):
+        return self._length
+
+    @property
+    def total_return(self):
+        return self._return
+
+    def end_episode(self):
+        self._return = 0
+        self._length = 0
+
 class Controller(object):
     def __init__(self, action_space, state_size, history_length = 1):
         self._session           = None
@@ -14,8 +35,9 @@ class Controller(object):
         self._history        = History(duration=history_length, state_size=state_size)
 
         # tracking variables
-        self._last_action = None
-        self._frame_count = 0
+        self._last_action   = None
+        self._frame_count   = 0
+        self._track_episode = EpisodeTracker()
 
     # getter properties
     @property
@@ -48,6 +70,8 @@ class Controller(object):
         if not test:
             self._frame_count += 1
 
+        self._track_episode(reward)
+
         action = self._last_action
         #TODO we can cache this
         last_state = self.full_state
@@ -55,6 +79,13 @@ class Controller(object):
         if state is not None:
             state = self._history.observe(state)
         else:
+            # push summaries
+            if not test and self._summary_writer is not None:
+                feed = {self._epsum_lg: self._track_episode.duration, self._epsum_rt: self._track_episode.total_return}
+                smr  = self._session.run(self._epsum, feed_dict = feed)
+                self._summary_writer.add_summary(smr, self._frame_count)
+
+            self._track_episode.end_episode()
             state = None
             self._last_action = None
             self._history.clear()
@@ -81,5 +112,16 @@ class Controller(object):
 
         self._session        = session
         self._summary_writer = logger
+
+        with tf.name_scope("episode_summaries"):
+            rt  = tf.placeholder(tf.float32, shape=[], name="return_")
+            lg  = tf.placeholder(tf.int32,   shape=[], name="duration_")
+            rts = tf.summary.scalar("return", rt)
+            lgs = tf.summary.scalar("duration", lg)
+            sms = tf.summary.merge([rts, lgs])
+            self._epsum = sms
+            self._epsum_rt = rt
+            self._epsum_lg = lg
+
         # this might be a bit wasteful! I don't know.
         self._session.run([tf.global_variables_initializer()])
